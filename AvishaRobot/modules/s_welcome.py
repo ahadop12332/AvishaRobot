2f613d375de03379cde4b35641d3eba4fabb4c84
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-07-26 15:13:09 +0530

    Update s_welcome.py

--- AvishaRobot/modules/s_welcome.py
+++ AvishaRobot/modules/s_welcome.py
@@ -127,7 +127,7 @@ async def greet_group(_, member: ChatMemberUpdated):
 ● ᴜsᴇʀ ɪᴅ ➥ {user.id}
 ● ᴜsᴇʀɴᴀᴍᴇ ➥ @{user.username}
 
-❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [ɴ ᴏ ᴠ ᴧ ♡゙](https://t.me/PhoenixXsupport)**
+〄 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [ɴ ᴏ ᴠ ᴧ ♡゙](https://t.me/PhoenixXsupport)**
 ▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
 """,
             reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton (f"ᴠɪᴇᴡ ᴜsᴇʀ", url=f"https://t.me/{user.username}")]])
