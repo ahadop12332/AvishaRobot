2a4279a3cde87dee3d81b3a9bcf5bbbd5fdc5a97
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-07-31 15:07:27 +0530

    Update gpt.py

--- AvishaRobot/modules/gpt.py
+++ AvishaRobot/modules/gpt.py
@@ -30,7 +30,7 @@ async def chat_gpt(event):
 
                 # Add signature to the answer
                 answer = result.get("answer", "⬤ ɴᴏ ᴀɴsᴡᴇʀ ʀᴇᴄᴇɪᴠᴇᴅ ғʀᴏᴍ ᴄʜᴀᴛ ᴀɪ.")
-                signature = "\n\n❖ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➥ [๛ᴀ ᴠ ɪ s ʜ ᴀ ♡゙](https://t.me/the_friendz)"
+                signature = "\n\n𖣐 ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➥ [ʟ ᴜ ᴄ ʏ • / ‹𝟹](https://t.me/PhoenixXsupport)"
                 reply_message = answer + signature
 
                 # Edit the "Please wait" message with the final answer
