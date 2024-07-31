38137142a4d66bc793622517f494726fb60381db
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-07-31 15:42:14 +0530

    Update v_song.py

--- AvishaRobot/modules/v_song.py
+++ AvishaRobot/modules/v_song.py
@@ -89,7 +89,7 @@ async def ytmusic(client, message: Message):
         return
     c_time = time.time()
     file_stark = f"{ytdl_data['id']}.mp4"
-    capy = f"❖ **sᴏɴɢ ᴛɪᴛʟᴇ ➥** [{thum}]({mo})\n\n● **ᴄʜᴀɴɴᴇʟ ➥** {thums}\n● **sᴇᴀʀᴄʜᴇᴅ ➥** {urlissed}\n\n𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ʟ ᴜ ᴄ ʏ • / ‹𝟹"
+    capy = f"𖣐 **sᴏɴɢ ᴛɪᴛʟᴇ ➥** [{thum}]({mo})\n\n● **ᴄʜᴀɴɴᴇʟ ➥** {thums}\n● **sᴇᴀʀᴄʜᴇᴅ ➥** {urlissed}\n\n𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ʟ ᴜ ᴄ ʏ • / ‹𝟹"
     await client.send_video(
         message.chat.id,
         video=open(file_stark, "rb"),
