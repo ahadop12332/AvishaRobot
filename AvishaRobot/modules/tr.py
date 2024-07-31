1b7ac2539a4b4db47d012151aabf61e29349ef02
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-07-31 16:42:32 +0530

    Update tr.py

--- AvishaRobot/modules/tr.py
+++ AvishaRobot/modules/tr.py
@@ -29,7 +29,7 @@ async def translate(_, message) -> None:
     translation = await trans(to_translate, sourcelang=source, targetlang=dest)
     reply = (
         f"⬤ ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} ᴛᴏ {dest} ➥\n\n"
-        f"♥︎ {translation.text}\n\n❖ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴋᴧɪ ꭙ ꝛᴏʙσᴛ™ ♡゙"
+        f"♥︎ {translation.text}\n\n𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ ˹ ᴋᴧɪ ꭙ ꝛᴏʙσᴛ™ ♡゙"
     )
     await message.reply_text(reply)
   
