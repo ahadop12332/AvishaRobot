2f7871bb700ce50927909c369ae4cc74ce43a174
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-08-03 00:11:41 +0530

    Update __main__.py

--- AvishaRobot/__main__.py
+++ AvishaRobot/__main__.py
@@ -347,7 +347,8 @@ def start(update: Update, context: CallbackContext):
             lol.edit_text("0️⃣")
             time.sleep(0.3)
             lol.edit_text("ʟᴇᴛ's ɢᴏ...♥︎")
-            lol.delete()            
+            lol.delete()
+                
             update.effective_message.reply_photo(random.choice(NYKAA),PM_START_TEXT.format(escape_markdown(first_name),BOT_NAME,sql.num_users(),sql.num_chats()),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
