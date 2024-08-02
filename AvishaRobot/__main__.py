3521ddc44ecfb2eb61957505b31314d7d0f486a0
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-08-02 23:58:00 +0530

    Update __main__.py

--- AvishaRobot/__main__.py
+++ AvishaRobot/__main__.py
@@ -323,7 +323,7 @@ def start(update: Update, context: CallbackContext):
             lol = update.effective_message.reply_text(
                 PM_START_TEX.format(usr.first_name), parse_mode=ParseMode.MARKDOWN
             )
-                        time.sleep(0.4)
+            time.sleep(0.4)
             lol.edit_text("🔟")
             time.sleep(0.3)
             lol.edit_text("9️⃣")
