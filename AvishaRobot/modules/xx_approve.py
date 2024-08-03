2158842ece6cf558b73ff2d3d7d08c2fb43fea7c
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-08-03 13:11:30 +0530

    Update xx_approve.py

--- AvishaRobot/modules/xx_approve.py
+++ AvishaRobot/modules/xx_approve.py
@@ -24,7 +24,7 @@ async def autoapprove(client: app, message: ChatJoinRequest):
     user = message.from_user  # User
     print(f"✦ {user.first_name} ᴊᴏɪɴᴇᴅ ♥︎")  # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    if APPROVED == "on":
+    if APPROVED == "off":
         await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title),reply_markup=InlineKeyboardMarkup(EVAA),)
 
 
