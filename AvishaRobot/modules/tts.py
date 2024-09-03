d6afeb0bc780c1e49416863e17d4f978cac77101
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-03 21:02:00 +0530

    Update tts.py

--- AvishaRobot/modules/tts.py
+++ AvishaRobot/modules/tts.py
@@ -7,7 +7,7 @@ def text_to_speech(client, message):
     text = message.text.split(' ', 1)[1]
     tts = gTTS(text=text, lang='hi')
     tts.save('avisha.mp3')
-    client.send_audio(message.chat.id, 'avisha.mp3')
+    client.send_audio(message.chat.id, 'ʟᴜᴄʏ.mp3')
   
 __mod_name__ = "ɢᴛᴛs"
 
