147b67505bacb6ea44b1428958b051576183962b
LucyXDbot <lucymorningstar.9783@gmail.com>
2024-09-05 10:34:59 +0530

    Update s_welcome.py

--- AvishaRobot/modules/s_welcome.py
+++ AvishaRobot/modules/s_welcome.py
@@ -7,10 +7,9 @@ from pyrogram.types import *
 from logging import getLogger
 
 from AvishaRobot import pbot as app
-
 from AvishaRobot.database.wel_db import *
 
-COMMAND_HANDLER = ". /".split() # COMMAND HANDLER
+COMMAND_HANDLER = [".", "/"]  # Command handlers
 
 LOGGER = getLogger(__name__)
 
@@ -37,88 +36,74 @@ def welcomepic(pic, user, chat, id, uname):
     background = Image.open("AvishaRobot/resources/bg.jpg")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
-    pfp = pfp.resize(
-        (450, 450)
-    ) 
+    pfp = pfp.resize((450, 450)) 
     draw = ImageDraw.Draw(background)
     font = ImageFont.truetype('AvishaRobot/resources/SwanseaBold-D0ox.ttf', size=44)
     welcome_font = ImageFont.truetype('AvishaRobot/resources/SwanseaBold-D0ox.ttf', size=90)
     draw.text((65, 250), f'NAME : {unidecode(user)}', fill=(255, 255, 255), font=font)
     draw.text((65, 340), f'ID : {id}', fill=(255, 255, 255), font=font)
-    draw.text((65,430), f"USERNAME : {uname}", fill=(255,255,255),font=font)
+    draw.text((65, 430), f"USERNAME : {uname}", fill=(255,255,255), font=font)
     pfp_position = (767, 133)  
     background.paste(pfp, pfp_position, pfp)  
-    background.save(
-        f"downloads/welcome#{id}.png"
-    )
+    background.save(f"downloads/welcome#{id}.png")
     return f"downloads/welcome#{id}.png"
 
-CUTE = """**
 @app.on_message(filters.command("zwelcome", COMMAND_HANDLER) & ~filters.public)
 async def auto_state(_, message):
     usage = "**❅ ᴜsᴀɢᴇ ➥ **/zwelcome [ᴇɴᴀʙʟᴇ|ᴅɪsᴀʙʟᴇ]"
+    
     if len(message.command) == 1:
         return await message.reply_text(usage)
+    
     chat_id = message.chat.id
-    user = await app.get_chat_member(message.chat.id, message.from_user.id)
-    if user.status in (
-        enums.ChatMemberStatus.ADMINISTRATOR,
-        enums.ChatMemberStatus.OWNER,
-    ):
-      A = await wlcm.find_one({"chat_id" : chat_id})
-      state = message.text.split(None, 1)[1].strip()
-      state = state.lower()
-      if state == "enable":
-        if A:
-           return await message.reply_text("๏ sᴘᴇᴄɪᴀʟ ᴡᴇʟᴄᴏᴍᴇ ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ")
-        elif not A:
-           await add_wlcm(chat_id)
-           await message.reply_text(f"๏ ᴇɴᴀʙʟᴇᴅ sᴘᴇᴄɪᴀʟ ᴡᴇʟᴄᴏᴍᴇ ɪɴ ➥ {message.chat.title}")
-      elif state == "disable":
-        if not A:
-           return await message.reply_text("๏ sᴘᴇᴄɪᴀʟ ᴡᴇʟᴄᴏᴍᴇ ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ")
-        elif A:
-           await rm_wlcm(chat_id)
-           await message.reply_text(f"๏ ᴅɪsᴀʙʟᴇᴅ sᴘᴇᴄɪᴀʟ ᴡᴇʟᴄᴏᴍᴇ ɪɴ ➥ {message.chat.title}")
-      else:
-        await message.reply_text(usage)
-    else:
-        await message.reply("๏ ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
-** """
+    user = await app.get_chat_member(chat_id, message.from_user.id)
+    
+    if user.status not in (enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.OWNER):
+        return await message.reply("๏ ᴏɴʟʏ ᴀᴅᴍɪɴs ᴄᴀɴ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ")
+    
+    A = await wlcm.find_one({"chat_id": chat_id})
+    state = message.text.split(None, 1)[1].strip().lower()
 
+    if state == "enable":
+        if A:
+            return await message.reply_text("๏ sᴘᴇᴄɪᴀʟ ᴡᴇʟᴄᴏᴍᴇ ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ")
+        await add_wlcm(chat_id)
+        await message.reply_text(f"๏ ᴇɴᴀʙʟᴇᴅ sᴘᴇᴄɪᴀʟ ᴡᴇʟᴄᴏᴍᴇ ɪɴ ➥ {message.chat.title}")
+    
+    elif state == "disable":
+        if not A:
+            return await message.reply_text("๏ sᴘᴇᴄɪᴀʟ ᴡᴇʟᴄᴏᴍᴇ ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ")
+        await rm_wlcm(chat_id)
+        await message.reply_text(f"๏ ᴅɪsᴀʙʟᴇᴅ sᴘᴇᴄɪᴀʟ ᴡᴇʟᴄᴏᴍᴇ ɪɴ ➥ {message.chat.title}")
+    
+    else:
+        await message.reply_text(usage)
 
 @app.on_chat_member_updated(filters.group, group=-3)
 async def greet_group(_, member: ChatMemberUpdated):
     chat_id = member.chat.id
-    #A = await wlcm.find_one({"chat_id" : chat_id})
-    #if not A:
-      # return
-    if (
-        not member.new_chat_member
-        or member.new_chat_member.status in {"restricted"}
-        or member.old_chat_member
-    ):
+
+    if not member.new_chat_member or member.new_chat_member.status == "restricted":
         return
+
     user = member.new_chat_member.user if member.new_chat_member else member.from_user
     try:
-        pic = await app.download_media(
-            user.photo.big_file_id, file_name=f"pp{user.id}.png"
-        )
+        pic = await app.download_media(user.photo.big_file_id, file_name=f"pp{user.id}.png")
     except AttributeError:
         pic = "HuTao/resources/profilepic.jpg"
-    if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
+
+    if temp.MELCOW.get(f"welcome-{chat_id}") is not None:
         try:
-            await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
+            await temp.MELCOW[f"welcome-{chat_id}"].delete()
         except Exception as e:
             LOGGER.error(e)
+
     try:
-        welcomeimg = welcomepic(
-            pic, user.first_name, member.chat.title, user.id, user.username
-        )
-        temp.MELCOW[f"welcome-{member.chat.id}"] = await app.send_photo(
-            member.chat.id,
+        welcomeimg = welcomepic(pic, user.first_name, member.chat.title, user.id, user.username)
+        temp.MELCOW[f"welcome-{chat_id}"] = await app.send_photo(
+            chat_id,
             photo=welcomeimg,
-            caption= f"""
+            caption=f"""
 **ㅤㅤㅤ◦•●◉✿ ᴡᴇʟᴄᴏᴍᴇ ʙᴀʙʏ ✿◉●•◦
 ▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
 
@@ -130,16 +115,15 @@ async def greet_group(_, member: ChatMemberUpdated):
 𖣐 ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➥ [ʟ ᴜ ᴄ ʏ • / ‹𝟹゙](https://t.me/PhoenixXsupport)**
 ▰▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▰
 """,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton (f"ᴠɪᴇᴡ ᴜsᴇʀ", url=f"https://t.me/{user.username}")]])
-
-            )
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton(f"ᴠɪᴇᴡ ᴜsᴇʀ", url=f"https://t.me/{user.username}")]]
+            ),
+        )
     except Exception as e:
         LOGGER.error(e)
+
     try:
         os.remove(f"downloads/welcome#{user.id}.png")
         os.remove(f"downloads/pp{user.id}.png")
     except Exception as e:
-        return 
-
-
-#####
+        LOGGER.error(e)
